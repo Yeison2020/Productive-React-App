eb44ac237923cb64aa8ba6b26866e68a8040c7af
James Brownie <jamespbrownie@gmail.com>
2021-10-27 11:13:33 -0500

    card paths work now

--- src/components/GoalCard.js
+++ src/components/GoalCard.js
@@ -1,34 +1,39 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import {useParams} from 'react-router-dom'
 
-function GoalCard({ goal }) {
+function GoalCard() {
+
+  const [goal, setGoal] = useState({})
+  // const [isLoaded, setIsloaded] = useState(false);
 
   const id = useParams().id
 
   useEffect(()=> {
     console.log('hi')
     console.log(id)
-    fetch(`http://localhost:3000/${id}`)
+    fetch(`http://localhost:3000/goals/${id}`)
     .then(res => res.json())
     .then(data => {
         console.log(data)
-        setCake(data)
-        setIsLoaded(true)
+        setGoal(data)
+        // setIsLoaded(true)
+        window.scrollTo(0, 0)
     });
-},[]);
+},[id]);
 
 
-
-
+// if(!isLoaded) return <h1>Loading</h1>
+const {name, urgency, category, completed, dueDate, notes, totalTime} = goal
   return (
     <div class="goalCard">
-      <h3>{goal.name}</h3>
-      <p>Urgency: {goal.urgency}</p>
-      <p>Category: {goal.category}</p>
-      <p>Completed: {goal.completed ? "Yes" : "No"}</p>
-      <p>Due Date: {goal.dueDate}</p>
-      <p>Notes: {goal.notes}</p>
-      <p>Total Time: {goal.totalTime}</p>
+      <h3>{name}</h3>
+      <p>Urgency: {urgency}</p>
+      <p>Category: {category}</p>
+      <p>Completed: {completed ? "Yes" : "No"}</p>
+      <p>Due Date: {dueDate}</p>
+      <p>Notes: {notes}</p>
+      <p>Total Time: {totalTime}</p>
+      <h1>testing</h1>
     </div>
   );
 }
